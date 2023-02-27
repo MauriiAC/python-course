a788f7d5dd7aaa68719767deaa6025b2e8e137ee
MauriiAC <mauriciocuello91@gmail.com>
2023-02-27 10:07:04 +0100

    ejemlpos de str e int agregados

--- 01-numbers.py
+++ 01-numbers.py
@@ -1,8 +1,12 @@
-## - Tipos de numeros: int, float, complex
-
+# ## - Tipos de numeros: int, float, complex
+# print(type( 5 ))
+# print(type( 5.0 ))
+# print(type( 5j ))
 
 ## - Operaciones matemáticas
+# print(7 // 4)
+# print(6 % 4)
 
 
 ## - Precedencia
-
+# print((5 + 2)**3 // 4 - 10)
--- 02-strings.py
+++ 02-strings.py
@@ -1,7 +1,28 @@
-## - Sintaxis - Comillas
+# ## - Sintaxis - Comillas
+# print("Hola Alumnos")
+# print('Hola Alumnos')
 
+# print(5)
+# print("5")
+
+# print("5" == 5)
 
 ## - Impresión de strings - Caracteres especiales
+# print("Bienvenidos a todos a este curso\nTemario\nClase1: tipo de datos")
+# print("Nombre\tApellido\tEdad")
+
+# print("En python se usan comillas para representar un string, por ej. \"Hola Mundo\"")
+# print('En python se usan comillas para representar un string, por ej. "Hola Mundo"')
+
+# print("Colcamos \\n para generar un salto de linea")
+# print(r"Colcamos \n para generar un salto de linea")
+
+# ## - Triple comilla
+# print("""Este es un ejemplo de un parrafo muy
+# largo donde voy a escribir muchas lineas
+# de texto""")
 
 
-## - Concatenación
+# ## - Concatenación
+# print("hola " + "mundo" + "!")
+
