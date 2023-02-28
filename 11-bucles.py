7a413383833f1e43f1ebb54f3ec57cbca7d16d47
MauriiAC <mauriciocuello91@gmail.com>
2023-02-28 13:59:08 +0100

    se agregaron los ejemplos de funciones

--- 11-bucles.py
+++ 11-bucles.py
@@ -90,25 +90,133 @@
 # 10  
 # Dado el diccionario "respuestas", escribir un programa que saque por pantalla el mensaje "chat activo: " y según lo que el usuario ingrese devolver el valor correspondiente a esa clave, si la clave no existe imprimr 'lo siento, no te entendí, me lo dices de otra forma?'
 # Finalizar el chat si el segundo valor de la tupla es True
+respuestas = {
+    'hola chatbot': ('hola, como estás?', False),
+    'buen dia': ('muy buenos días, espero te encuentres muy bien', False),
+    'adios': ('hasta la próxima compañero', True),
+    'cuanto es 2 + 2?': (4, False)
+}
 
+# print("Chat activo:")
+# pregunta = input("> ")
 
+# # nos aseguramos que el primer input sea valido
+# while pregunta not in respuestas:
+#     print("'lo siento, no te entendí, me lo dices de otra forma?'")
+#     pregunta = input("> ")
 
-## - for, sintaxis
+# # algoritmo principal
+# while (respuestas[pregunta][1] != True):
+#     print(respuestas[pregunta][0])
+#     pregunta = input("> ")
+#     while pregunta not in respuestas:
+#         print("'lo siento, no te entendí, me lo dices de otra forma?'")
+#         pregunta = input("> ")
+# else:
+#     # decimos adios
+#     print(respuestas[pregunta][0])
 
+# texto = input('Chat activo: ')
+# while True:
+#     respuesta = respuestas.get(texto)
+#     if respuesta:
+#         if respuesta[1]:
+#             print(respuesta[0])
+#             break
+#         print(respuesta[0])
+#         texto = input(">> ")
+#     else:
+#         texto = input("Lo siento, no te entendi, me lo dices de otra forma?")
 
-## - desempaquetado en variable iteradora
+# ## - for, sintaxis
+
+# lista = [1,2,3,4,5]
+# lista_dobles = []
+
+# for i in lista:
+#     lista_dobles.append(i*2)
+
+# print(lista_dobles)
+
+# ## - desempaquetado en variable iteradora, r4
+# lista_personas = [("Mauricio", "Cuello", 30), ("Lionel", "Messi", 34), ("Cristiano", "Ronaldo", 34), ("Pepe", "Mujica", 40)]
+
+# for nombre, apellido, edad in lista_personas:
+#     print(nombre)
+#     print(edad)
 
 
 ## - enumerate, Ejer #23
+# 23 
+# Dada la lista de paises dentro de mockups.py armar un programa que obtenga la posición del pais con el nombre más largo
 
+# paises = ['Canada', 'USA', 'Mexico', 'Australia', 'Argentina', 'Corea del Sur', 'China', 'India', 'Singapur', 'Croacia']
+# # print(list(enumerate(paises)))
+
+# candidato = paises[0]
+# pos_candidato = 0
+
+# for pos, pais in enumerate(paises):
+    
+#     if len(pais) > len(candidato):
+#         candidato = pais
+#         pos_candidato = pos
+
+# print(pos_candidato)
 
 ## - recorrer distintas coleciones, str, set, dict, list
 
+# for i in "Mauricio":
+#     print(i)
 
 ## - range
 
+# for i in range(5, 15, 2):
+#     print(i)
 
-## - else, break, continue
 
+# ## - else, break, continue
+# mi_lista = [3,5,90,12,11]
+
+# for i in mi_lista:
+#     if i == 9:
+#         break
+# else:
+#     print("No hay ningún 9")
+
+# print("Se finalizó el programa")
 
 ## - Ejercicio #9
+# 9
+# Dada la lista personas en mockups.py, pedir al usuario que ingrese una letra y devolver 
+# una lista con los nombres de las personas que su nombre empieza con esa letra
+# En el caso que no haya ninguno, devolver que no existe ningún nombre con esa letra 
+personas = [
+    {"id":1,"first_name":"Catha","last_name":"Carlton","city":"Qingshandi","email":"ccarlton0@twitter.com","gender":"Polygender"},
+    {"id":2,"first_name":"Toddie","last_name":"Ibeson","city":"San Juan","email":"tibeson1@freewebs.com","gender":"Bigender"},
+    {"id":3,"first_name":"Ashlee","last_name":"McAuslan","city":"São Jerônimo","email":"amcauslan2@pcworld.com","gender":"Polygender"},
+    {"id":4,"first_name":"Julie","last_name":"Fischer","city":"Lasusua","email":"jfischer3@ucoz.com","gender":"Agender"},
+    {"id":5,"first_name":"Manda","last_name":"Mapis","city":"Sindang","email":"mmapis4@foxnews.com","gender":"Non-binary"},
+    {"id":6,"first_name":"Noami","last_name":"Rubanenko","city":"Siemianowice Śląskie","email":"nrubanenko5@geocities.com","gender":"Genderfluid"},
+    {"id":7,"first_name":"Daffi","last_name":"Wherton","city":"Kamirenjaku","email":"dwherton6@privacy.gov.au","gender":"Bigender"},
+    {"id":8,"first_name":"Tamma","last_name":"Worsham","city":"Batang","email":"tworsham7@globo.com","gender":"Male"},
+    {"id":9,"first_name":"Gibby","last_name":"Blacktin","city":"Makarov","email":"gblacktin8@mac.com","gender":"Agender"},
+    {"id":10,"first_name":"Locke","last_name":"Pirdy","city":"Ketanggungan","email":"lpirdy9@wix.com","gender":"Polygender"},
+    {"id":11,"first_name":"Dorree","last_name":"Claypool","city":"Laborie","email":"dclaypoola@un.org","gender":"Female"},
+    {"id":12,"first_name":"Jermaine","last_name":"Duplan","city":"Chemin Grenier","email":"jduplanb@skype.com","gender":"Polygender"},
+    {"id":13,"first_name":"Kliment","last_name":"Divill","city":"Baochang","email":"kdivillc@tamu.edu","gender":"Agender"},
+    {"id":14,"first_name":"Bernice","last_name":"O'Hartnett","city":"Askainen","email":"bohartnettd@tripod.com","gender":"Genderqueer"},
+    {"id":15,"first_name":"Teirtza","last_name":"Summerlee","city":"Babakanbungur","email":"tsummerleee@scientificamerican.com","gender":"Agender"}
+    ]
+
+letra = input("Ingrese una letra: ")
+nombres = set()
+
+for persona in personas:
+    if letra.capitalize() == persona["first_name"][0]:
+        nombres.add(persona["first_name"])
+
+if len(nombres) > 0:
+  print(list(nombres))
+else:
+  print("No existe un nombre con esa letra")
--- 12-funciones.py
+++ 12-funciones.py
@@ -1,55 +1,219 @@
 ## - integradas vs propias
+# print(type(len("Mauricio")))
 
+# ## - sintaxis
+# def saluda():
+#     print("Buenos días!")
 
-## - sintaxis
+# ## - definición vs invocación
+# saluda()
 
-
-## - definición vs invocación
-
-
-## - reutilización de código
-
-
-## - definición vs invocación
-
-
+## - reutilización de código 
 ## - uso de parametros
-
-
 ## - generalización
 
+# def calcula(op, a, b):
+#   if op == 'sum':
+#     print(f'El resultado de sumar {a} + {b}: {a+b}')
+#   elif op == 'res':  
+#     print(f'El resultado de restar {a} - {b}: {a-b}')
+#   elif op == 'mul':
+#     print(f'El resultado de multiplicar {a} * {b}: {a*b}')
+#   elif op == 'div':
+#     print(f'El resultado de dividir {a} / {b}: {a/b}')
 
-## - return
+
+# print("Haciendo algunas cosas...")
+
+# calcula('sum',2,11)
+
+# print("haciendo mas cosas importantes...")
+
+# calcula('res', 7, 2)
+
+# calcula('mul',3,8)
 
 
-## - ambito de variable (scope a nivel de función)
 
+# ## - return
+# def calcula(op, a, b):
+#   if op == 'sum':
+#     return a + b
+#     print("Se sumaro los valores") # No se lee porque el return finaliza la ejecución
+#   elif op == 'res':  
+#     return a - b
+#   elif op == 'mul':
+#     return a * b
+#   elif op == 'div':
+#     return a / b
+  
+# print(calcula('sum', 2,8))
 
-## - return tupla implicita
+# ## - ambito de variable (scope a nivel de función)
+# nombre = "Jorge"
+
+# def saluda_con_nombre():
+#     nombre = "Mauricio"
+#     return f'Hola {nombre}'
+
+# print(saluda_con_nombre())
+
+# print(nombre)
+
+# ## - return tupla implicita
+# def retorna_valores():
+#     return 2, 4, 8
+
+# print(retorna_valores())
 
 
 ## - Ejercicios #24 a #28
+# 24
+# Crear una función que reciba un número y una lista, e imprima {"Todos los valores son divisibles por {numero}"} si son todos divisibles, o {"Hay valores que no se pueden dividir por {numero}"} en caso contrario
 
+
+# def todos_divisibles(lista, numero):
+    
+#     for i in lista:
+#         if i % numero != 0:
+#             return f'No son todos divisibles por {numero}'
+#     return f'Son todos divisibles por {numero}'
+
+# print(todos_divisibles([2,4,6,8,10,13], 2))
+
+# # 26
+# # Crear una función que reciba una tupla de palabras y retorne una frase uniendo esas palabras separadas por un espacio
+
+# def crear_frase(palabras):
+#     resultado = ""
+#     for palabra in palabras:
+#         resultado += palabra + " "
+#     return resultado
+
+# print(crear_frase(["Hola", "Mundo.", "Que", "disfruten", "su", "dia"]))
 
 ## - Argumentos por nombre vs por posición
+# def saluda(nombre, apellido):
+    
+#     return f'Hola, me llamo {nombre} {apellido}'
+
+# print(saluda("Mauricio", "Cuello"))
+# print(saluda("Cuello", "Mauricio"))
+
+# print(saluda(nombre="Mauricio", apellido="Cuello"))
+# print(saluda(apellido="Cuello", nombre="Mauricio"))
 
 
 ## - Parametros por defecto
+# def saluda(nombre, apellido=""):
+    
+#     return f'Hola, me llamo {nombre} {apellido}'
+
+# print(saluda("Mauricio"))
+# print(saluda("Mauricio", "Cuello"))
+# print(saluda(nombre="Mauricio"))
+
+# def resta(a=None, b=None):
+    
+#     if a and b:
+#       return a - b
+#     else:
+#        return 'debe ingresar ambos datos'
+
+# print(resta(5))
 
 
 ## - Pasaje de parametro por valor vs por referencia
+mi_nombre = "mauricio"
+mi_edad = 32
+mis_familiares = ["Juan", "Abril", "Mariela", "Jesica", "Roberto"]
+
+# def cambia_nombre(nombre: str):
+#     nombre = nombre.capitalize()
+#     print('local: ', nombre)
+
+# cambia_nombre(mi_nombre)
+# print('global:', mi_nombre)
+
+# def cambia_familiar(lista: list):
+    
+#     lista.append("Hanna")
+#     print('local:',lista)
+
+# cambia_familiar(mis_familiares)
+# print('global: ', mis_familiares)
 
 
 ## - r1 a r9 en examples.py
+# import copy
 
+# mi_lista = [[2, 8, -5, 24],[1, 3, 5, 7]]
+
+# mi_numero = 5
+
+# mi_lista2 = copy.deepcopy(mi_lista)
+# mi_lista3 = mi_lista
+
+# def cambia_valores(matriz):
+#     for fila in matriz:
+#         fila.append(0)
+
+# cambia_valores(mi_lista3)
+
+# print(mi_lista)
+# print(mi_lista2)
+# print(mi_lista3)
 
 ## - Argumentos indeterminados
 
+# def suma(*args):
+    
+#     return sum(args)
+
+# print(suma(5,10,40,20,3))
+
+# def suma(**kwargs):
+    
+#     return sum(kwargs.values())
+
+# print(suma(a=10, b=-5, c=8, d=-1))
+
+# def resultado(nombre, *args, **kwargs):
+    
+#     print(f'Hola {nombre}! estas son tus notas')
+
+#     for nota in args:
+#         print(f'Nota: {nota}')
+
+#     print(f'Le enviaremos una copia a {list(kwargs.values())}')
+
+
+# resultado("Mauricio", 4, 8, 3, 9, 10, papa="Roberto", mama="Mariela")
 
 ## - Ejercicio #29
 
+# TAREA
 
 ## - Recursividad
 
+# def cuenta(numero):
+#     numero -= 1
 
-## - Ejercicios #30 a #35 
+#     if numero > 0:
+#         print(f'> {numero}')
+#         cuenta(numero)
+#     else:
+#         print("Boom!!")
+
+# cuenta(5)
+
+def factorial(num):
+    
+    if num > 1:
+        return num * factorial(num-1)
+    else:
+        return num
+
+print(factorial(5))
+
+## - Ejercicios #30 a #35
