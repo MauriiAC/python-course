f3aead38ab76ec4bbced5b0c851cd97eec8c1882
MauriiAC <mauriciocuello91@gmail.com>
2023-03-01 13:08:04 +0100

    primeros ejemplos con poo

--- 14-poo.py
+++ 14-poo.py
@@ -1,4 +1,4 @@
-## - Fundamento de la POO 
+## - Fundamento de la POO
   # Bloques de código excesivamente grandes
   # Modularización
   # Código difícil de interpretar (cod spaguetti)
@@ -8,7 +8,7 @@
   # Complejo el control de la interacción con usuarios
   # Encapsulamiento
 
-## - Como se piensa con POO? id, caract, prop
+## - Como se piensa con POO? id, caract, funcionalidades
 
 
 ## - Diagrama de clase
@@ -19,7 +19,26 @@
 
 
 ## - primer ejemplo
+class Persona():
+    
+    def __init__(self, nom, ape, ed):
+        self.nombre = nom
+        self.apellido = ape
+        self.edad = ed
+    
+    def saluda(self):
+        return f'Hola, me llamo {self.nombre} {self.apellido}'
 
+messi = Persona("Lionel", "Messi", 35)
+cristiano = Persona("Cristiano", "Ronaldo", 38)
+riquelme = Persona("Roman", "Riquelme", 42)
+
+# print(messi.nombre)
+# print(messi.edad)
+# print(cristiano.apellido)
+
+print(messi.saluda())
+print(cristiano.saluda())
 
 ## - métodos de instancia
 
