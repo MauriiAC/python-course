8b5198801911b341a0e4af86ed471fe442b17b45
MauriiAC <mauriciocuello91@gmail.com>
2023-03-01 13:59:11 +0100

    primer ejemplo de poo

--- 14-poo.py
+++ 14-poo.py
@@ -19,32 +19,78 @@
 
 
 ## - primer ejemplo
-class Persona():
+# class Persona():
     
-    def __init__(self, nom, ape, ed):
-        self.nombre = nom
-        self.apellido = ape
-        self.edad = ed
+#     def __init__(self, nom, ape, ed, ami):
+#         self.nombre = nom
+#         self.apellido = ape
+#         self.edad = ed
+#         self.amigos = ami
     
-    def saluda(self):
-        return f'Hola, me llamo {self.nombre} {self.apellido}'
+#     def add_amigo(self, amigo):
+#         self.amigos.append(amigo)
 
-messi = Persona("Lionel", "Messi", 35)
-cristiano = Persona("Cristiano", "Ronaldo", 38)
-riquelme = Persona("Roman", "Riquelme", 42)
+#     def listar_amigos(self):
+#         mis_amigos = []
+#         for persona in self.amigos:
+#             mis_amigos.append(persona.nombre)
+#         lista_nombres = ', '.join(mis_amigos)
+#         return f'Soy amigo de {lista_nombres}'
 
-# print(messi.nombre)
-# print(messi.edad)
-# print(cristiano.apellido)
+#     def saluda(self):
+#         return f'Hola, me llamo {self.nombre} {self.apellido}'
+    
+# messi = Persona("Lionel", "Messi", 35, None)
+# cristiano = Persona("Cristiano", "Ronaldo", 38, None)
+# riquelme = Persona("Roman", "Riquelme", 42, None)
 
-print(messi.saluda())
-print(cristiano.saluda())
+# print(Persona.saluda(messi))
+
+# mauricio = Persona("Mauricio", "Cuello", 32, [messi, cristiano])
+
+# print(riquelme.listar_amigos())
+
+# # print(messi.nombre)
+# # print(messi.edad)
+# # print(cristiano.apellido)
+
+# # print(messi.saluda())
+# # print(cristiano.saluda())
+# print(mauricio.listar_amigos())
+# mauricio.add_amigo(riquelme)
+# print(mauricio.listar_amigos())
+
 
 ## - métodos de instancia
+#     def saluda(self):
+#         return f'Hola, me llamo {self.nombre} {self.apellido}'
 
+## - atributo de clase vs atributo de instancia, metodos con parametros
+class Perro():
+    
+    num_patas = 4
+    
+    def __init__(self, nombre, raza, edad):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+      
+    def __str__(self):
+        return f'{self.raza} de {self.edad} años'
+      
+    def ladrar(self, cant):
+        return 'guau'*cant
 
-## - atributo de clase vs atributo de instancia
+perro1 = Perro("cachito", "caniche", 2)
+perro2 = Perro("pepito", "dalmata", 4)
+# print(perro1.ladrar(10))
+# print(perro1.ladrar(2))
 
+# print(Perro.num_patas)
+# print(perro1.num_patas)
+
+print(perro1)
+print(perro2)
 
 ## - encapsulamiento, modificación controlada
 
@@ -52,5 +98,8 @@ print(cristiano.saluda())
 ## - relación entre clases
 
 
-## - 
+## - herencia
+
+
+## - polimorfismo
 
